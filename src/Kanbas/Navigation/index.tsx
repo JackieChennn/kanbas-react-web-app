238b122f507adccda8454457bc8b54139b37433c
Jackie Chen <cjc337993932@gmail.com>
2024-02-18 21:11:51 -0500

    Finished 3.1 Implementing Kanbas Navigation as a React.js component

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,32 +1,52 @@
-import { Link, useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {MdAccountCircle} from "react-icons/md";
+import {RiBook2Line} from "react-icons/ri";
+import {IoCalendarOutline} from "react-icons/io5";
+import {TfiDashboard} from "react-icons/tfi";
+import {BsInbox} from "react-icons/bs";
+import {SlClock} from "react-icons/sl";
+import {TbPresentationAnalytics} from "react-icons/tb";
+import {IoArrowForwardCircleOutline} from "react-icons/io5";
+import {AiOutlineQuestionCircle} from "react-icons/ai";
 import "./index.css";
-import {
-  FaTachometerAlt,
-  FaRegUserCircle,
-  FaBook,
-  FaRegCalendarAlt,
-} from "react-icons/fa";
+
 function KanbasNavigation() {
-  const links = [
-    { label: "Account", icon: <FaRegUserCircle className="fs-2" /> },
-    { label: "Dashboard", icon: <FaTachometerAlt className="fs-2" /> },
-    { label: "Courses", icon: <FaBook className="fs-2" /> },
-    { label: "Calendar", icon: <FaRegCalendarAlt className="fs-2" /> },
-  ];
-  const { pathname } = useLocation();
+
+  const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
+  const linkToIconMap: { [key: string]: JSX.Element } = {
+    "Account": <MdAccountCircle className="wd-icon"/>,
+    "Dashboard": <TfiDashboard className="wd-icon"/>,
+    "Courses": <RiBook2Line className="wd-icon"/>,
+    "Calendar": <IoCalendarOutline className="wd-icon"/>,
+    "Inbox": <BsInbox className="wd-icon"/>,
+    "History": <SlClock className="wd-icon"/>,
+    "Studio": <TbPresentationAnalytics className="wd-icon"/>,
+    "Commons": <IoArrowForwardCircleOutline className="wd-icon"/>,
+    "Help": <AiOutlineQuestionCircle className="wd-icon"/>,
+  }
+
+  const {pathname} = useLocation();
+
   return (
-    <ul className="wd-kanbas-navigation">
-      {links.map((link, index) => (
-        <li
-          key={index}
-          className={pathname.includes(link.label) ? "wd-active" : ""}
-        >
-          <Link to={`/Kanbas/${link.label}`}>
-            {link.icon} {link.label}
+      <div className="wd-kanbas-navbar">
+        <div className="list-group wd-kanbas-navigation" style={{width: 150}}>
+          <Link key={9} to='/Kanbas/Dashboard'
+                className='list-group-item d-flex flex-column neu-icon'>
+            <img className="img-fluid" src={require("./NEU_logo.png")}
+                 alt="NEU Logo"/>
           </Link>
-        </li>
-      ))}
-    </ul>
+          {links.map((link, index) => (
+              <Link
+                  key={index}
+                  to={`/Kanbas/${link}`}
+                  className={`list-group-item ${pathname.includes(link) && "active"} d-flex flex-column`}>
+                {linkToIconMap[link]}
+                {link}
+              </Link>
+          ))}
+        </div>
+      </div>
   );
 }
+
 export default KanbasNavigation;
