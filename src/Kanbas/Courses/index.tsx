9fa7f52aafc5f50651f9bc278ba116bd3f173e14
Jackie Chen <cjc337993932@gmail.com>
2024-02-19 14:31:36 -0500

    Finished 3.4 Implementing the Course Navigation as a React.js component

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,40 +1,43 @@
 import React from "react";
-import { useParams, Routes, Route, Navigate } from "react-router-dom";
+import {useParams, Routes, Route, Navigate} from "react-router-dom";
+import db from "../../Kanbas/Database"
 import courses from "../Database/courses.json";
 import ModuleList from "./Modules/List";
-import CourseNavigation from "./Navigation";
+import CourseNavigation from "./Navigation/CourseNavigation";
+import CourseTopNavigation from "./Navigation/CourseTopNavigation";
 
 function Courses() {
-  const { cid } = useParams();
-  const course = courses.find((course) => course._id === cid);
-  return (
-    <>
-      <h2>Courses {course?.name}</h2>
-      <CourseNavigation />
-      <div>
-        <div
-          className="overflow-y-scroll position-fixed bottom-0 end-0"
-          style={{ left: "320px", top: "50px" }}
-        >
-          <Routes>
-            <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<h1>Home</h1>} />
-            <Route path="Modules" element={<ModuleList />} />
-            <Route path="Piazza" element={<h1>Piazza</h1>} />
-            <Route path="Assignments" element={<h1>Assignments</h1>} />
-            <Route
-              path="Assignments/:assignmentId"
-              element={<h1>Assignment Editor</h1>}
-            />
-            <Route path="Grades" element={<h1>Grades</h1>} />
-          </Routes>
-        </div>
-      </div>
+  const {cid} = useParams();
+  const course = db.courses.find((course) => course._id === cid);
 
-      <pre>
-        <code>{JSON.stringify(course, null, 2)}</code>
-      </pre>
-    </>
+  return (
+      <div>
+        <CourseTopNavigation/>
+        <hr className="mb-3 mx-3"/>
+        <div>
+          <CourseNavigation/>
+          <div
+              className="overflow-y-scroll position-fixed bottom-0 end-0"
+              style={{
+                left: "320px",
+                top: "70px",
+              }}
+          >
+            <Routes>
+              <Route path="/" element={<Navigate to="Home"/>}/>
+              <Route path="Home" element={<h1>Home</h1>}/>
+              <Route path="Modules" element={<h1>Modules</h1>}/>
+              <Route path="Assignments" element={<h1>Assignments</h1>}/>
+              <Route
+                  path="Assignments/:assignmentId"
+                  element={<h1>Assignments -id</h1>}
+              />
+              <Route path="Grades" element={<h1>Grades</h1>}/>
+            </Routes>
+          </div>
+        </div>
+
+      </div>
   );
 }
 
