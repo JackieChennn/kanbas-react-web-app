539969e8ddbe164de8203f6a7b47060826c1d72c
Jackie Chen <cjc337993932@gmail.com>
2024-03-16 14:04:44 -0400

    Finished Lab 2.2.4 The Event Object

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 
 const Assignment4 = () => {
   function sayHello() {
     alert("Hello");
   }
+
   return (
       <>
         <h1>Assignment 4</h1>
+        <EventObject/>
         <PassingFunctions theFunction={sayHello}/>
         <PassingDataOnEvent/>
         <ClickEvent/>
