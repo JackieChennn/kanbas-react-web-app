7a783c63b5a5c957fcc8f92224f5ff0c099d27a3
Jackie Chen <cjc337993932@gmail.com>
2024-03-16 13:57:41 -0400

    Finished Lab 2.2.3 Passing Functions as Attributes

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
 
 const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
   return (
       <>
         <h1>Assignment 4</h1>
-        <ClickEvent/>
+        <PassingFunctions theFunction={sayHello}/>
         <PassingDataOnEvent/>
+        <ClickEvent/>
       </>
   );
 };
