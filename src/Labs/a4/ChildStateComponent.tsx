98604236c57867f616b46f168028f1715e19c271
Jackie Chen <cjc337993932@gmail.com>
2024-03-16 15:29:37 -0400

    Finished Lab 2.4.3 Counter Redux - Dispatching Events to Reducers

--- src/Labs/a4/ChildStateComponent.tsx
+++ src/Labs/a4/ChildStateComponent.tsx
@@ -6,10 +6,10 @@ function ChildStateComponent({counter, setCounter}:
   return (
       <div>
         <h3>Counter {counter}</h3>
-        <button onClick={() => setCounter(counter + 1)}>
+        <button onClick={() => setCounter(counter + 1)} className="btn btn-success">
           Increment
         </button>
-        <button onClick={() => setCounter(counter - 1)}>
+        <button onClick={() => setCounter(counter - 1)} className="btn btn-danger">
           Decrement
         </button>
       </div>
