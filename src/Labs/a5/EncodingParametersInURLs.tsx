74203dda9a7fe93fcef31275a09bbc16607bf6ef
Jackie Chen <cjc337993932@gmail.com>
2024-04-01 22:52:59 -0400

    Finished Lab 3.1

--- src/Labs/a5/EncodingParametersInURLs.tsx
+++ src/Labs/a5/EncodingParametersInURLs.tsx
@@ -18,11 +18,38 @@ function EncodingParametersInURLs() {
             onChange={(e) => setB(parseInt(e.target.value))}
         />
         <h3>Path Parameters</h3>
-        <a href={`http://localhost:4000/a5/add/${a}/${b}`}>
+        <a className="btn btn-primary" href={`http://localhost:4000/a5/add/${a}/${b}`}>
           Add {a} + {b}
         </a>
-        <a href={`http://localhost:4000/a5/subtract/${a}/${b}`}>
+        <a className="btn btn-danger" href={`http://localhost:4000/a5/subtract/${a}/${b}`}>
           Substract {a} - {b}
+        </a>
+        <h3>Query Parameters</h3>
+        <a className="btn btn-primary"
+           href={`http://localhost:4000/a5/calculator?operation=add&a=${a}&b=${b}`}>
+          Add {a} + {b}
+        </a>
+        <a className="btn btn-danger"
+           href={`http://localhost:4000/a5/calculator?operation=subtract&a=${a}&b=${b}`}>
+          Substract {a} - {b}
+        </a>
+        <h3>On Your Own - request's path</h3>
+        <a className="btn btn-primary"
+           href={`http://localhost:4000/a5/multiply/${a}/${b}`}>
+          Multiply {a} * {b}
+        </a>
+        <a className="btn btn-danger"
+           href={`http://localhost:4000/a5/divide/${a}/${b}`}>
+          Divide {a} / {b}
+        </a>
+        <h3>On Your Own - query string</h3>
+        <a className="btn btn-primary"
+           href={`http://localhost:4000/a5/calculator?operation=multiply&a=${a}&b=${b}`}>
+          Multiply {a} * {b}
+        </a>
+        <a className="btn btn-danger"
+           href={`http://localhost:4000/a5/calculator?operation=divide&a=${a}&b=${b}`}>
+          Divide {a} / {b}
         </a>
       </div>
   );
