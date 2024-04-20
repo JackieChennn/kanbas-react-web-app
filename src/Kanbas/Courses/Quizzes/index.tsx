d5d396abefe52bf8f261823519716d0bd56dc1e5
Jackie Chen <cjc337993932@gmail.com>
2024-04-20 15:49:45 -0400

    Publish/Unpublish button working

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import {
   FaBan,
   FaCaretDown,
@@ -11,110 +11,99 @@ import {
 import {Link, useNavigate, useParams} from "react-router-dom";
 import {KanbasState} from "../../store";
 import {deleteQuiz, setQuizzes} from "./reducer";
-
 import * as client from "./client";
 import {useDispatch, useSelector} from "react-redux";
 
 function Quizzes() {
-  const quizList = useSelector((state: KanbasState) =>
-      state.quizReducer.quizzes);
-  const quiz = useSelector((state: KanbasState) =>
-      state.quizReducer.quiz);
+  const quizList = useSelector((state: KanbasState) => state.quizReducer.quizzes);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const now = new Date();
-
-  const formatDate = (dateString: any) => {
-    const date = new Date(dateString);
-    const formatter = new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit' });
-    return formatter.format(date);
-  };
-  const getAvailability = (quiz: any) => {
-    const now = new Date();
-    const available_date = new Date(quiz.available_date);
-    const until_date = new Date(quiz.until_date);
-    if (now < available_date) {
-      return `Not available until ${formatDate(available_date)}`
-    } else if (now >= available_date && now < until_date) {
-      return `Available until ${formatDate(until_date)}`
-    } else {
-      return "Closed"
-    }
-  };
-  const handleNewQuizzes = () => {
-    navigate(`/Kanbas/Quizzes/new`);
-  };
-  const {quizId} = useParams();
+  const [activeMenu, setActiveMenu] = useState(null);
 
   useEffect(() => {
     client.findAllQuizzes().then(quizzes => dispatch(setQuizzes(quizzes)));
   }, [dispatch]);
 
-  const handleDeleteButton = (quiz: { _id: any; }) => {
+  const handleTogglePublish = (quiz: any) => {
+    const updatedQuiz = {...quiz, published: !quiz.published};
+
+    client.updateQuiz(updatedQuiz)
+    .then(() => {
+      const updatedQuizzes = quizList.map(q =>
+          q._id === quiz._id ? updatedQuiz : q
+      );
+      dispatch(setQuizzes(updatedQuizzes));  // Dispatch the action to update quizzes in Redux
+    })
+    .catch(err => {
+      console.error("Failed to update quiz", err);
+    });
+  };
+
+  const toggleMenu = (quizId: any) => {
+    setActiveMenu(activeMenu === quizId ? null : quizId);
+  };
+  const formatDate = (dateString: any) => {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+  };
+
+  const handleEditQuiz = (quizId: any) => {
+    navigate(`/Kanbas/Quizzes/${quizId}/edit`);
+  };
+
+  const handleDeleteQuiz = (quizId: any) => {
     const isConfirmed = window.confirm('Are you sure you want to delete?');
     if (isConfirmed) {
-      client.deleteQuiz(quiz._id).then(() => {
-        dispatch(deleteQuiz(quiz._id));
+      client.deleteQuiz(quizId).then(() => {
+        dispatch(deleteQuiz(quizId));
+        setActiveMenu(null);  // Close the menu
       });
     }
   };
+
   return (
       <>
         <div className="d-flex">
           <div className="flex-fill">
-      <span>
-        <span className="float-end">
-          <button className="btn btn-outline-secondary"><FaPlus/> Group</button>
-          <button className="btn btn-danger"
-                  onClick={() => handleNewQuizzes()}><FaPlus/> Quiz</button>
-          <button className="btn btn-outline-secondary"><FaEllipsisV/></button>
-        </span>
-        <input type="text" className="form-control w-50" id="assignmentSearch"
-               placeholder="Search for Quiz"/>
-      </span>
+          <span className="float-end">
+            <button className="btn btn-outline-secondary"><FaPlus/> Group</button>
+            <button className="btn btn-danger"
+                    onClick={() => navigate(`/Kanbas/Quizzes/new`)}><FaPlus/> Quiz</button>
+            <button className="btn btn-outline-secondary"><FaEllipsisV/></button>
+          </span>
+            <input type="text" className="form-control w-50" placeholder="Search for Quiz"/>
             <hr/>
             <ul className="list-group wd-modules">
-              <li className="list-group-item">
-                <div>
-                  <FaEllipsisV className="me-2"/> <FaCaretDown className="me-2"/> Assignment Quizzes
-                  <span className="float-end">
-              <span className="border me-3"
-                    style={{borderRadius: 40, width: 120, textAlign: "center"}}>40% of Total</span>
-              <FaPlusCircle className="ms-2"/><FaEllipsisV className="ms-2"/>
-            </span>
-                </div>
-                <ul className="list-group">
-                  {quizList.map((quiz, index) => (
-                      <li key={index} className="list-group-item">
-                        <FaEllipsisV className="me-2"/>
-                        <FaEdit className="me-2"/>
-                        {quiz.published ?
-                            <Link to={`/Kanbas/Quizzes/${quiz._id}`} style={{color: "black"}}>{quiz.quiz_name}</Link> :
-                            quiz.quiz_name}
-                        <span className="float-end">
-                  {quiz.published ?
-                      <FaCheckCircle className="text-success"/> :
+              {quizList.map((quiz, index) => (
+                  <li key={index} className="list-group-item">
+                    <FaEllipsisV className="me-2 clickable" onClick={() => toggleMenu(quiz._id)}/>
+                    {activeMenu === quiz._id && (
+                        <ul className="context-menu">
+                          <li onClick={() => handleEditQuiz(quiz._id)}>Edit</li>
+                          <li onClick={() => handleDeleteQuiz(quiz._id)}>Delete</li>
+                          <li onClick={() => handleTogglePublish(quiz)}>{quiz.published ? 'Unpublish' : 'Publish'}</li>
+                        </ul>
+                    )}
+                    <FaEdit className="me-2 clickable" onClick={() => handleEditQuiz(quiz._id)}/>
+                    <Link to={`/Kanbas/Quizzes/${quiz._id}`}
+                          style={{color: "black"}}>{quiz.quiz_name}</Link>
+                    <span className="float-end">
+                  {quiz.published ? <FaCheckCircle className="text-success"/> :
                       <FaBan className="text-secondary"/>}
-                          <FaEllipsisV className="ms-2"/>
-                </span><br/>
-                        <span style={{fontSize: 10}}>
-                  <a style={{color: "black"}}>{getAvailability(quiz)}</a> |
-                          Due {formatDate(quiz.due_date)} at 11:59pm | {quiz.points} pts | {quiz.questions.length} Questions
+                      <FaEllipsisV className="ms-2"/>
                 </span>
-                        <button className="float-end btn btn-outline-danger"
-                                onClick={() => handleDeleteButton(quiz)}>
-                          Delete
-                        </button>
-                      </li>
-                  ))}
-                </ul>
-              </li>
+                    <br/>
+                    <span style={{fontSize: 10}}>
+                  {quiz.published} |
+                  Due {formatDate(quiz.due_date)} at 11:59pm | {quiz.points} pts | {quiz.questions.length} Questions
+                </span>
+                  </li>
+              ))}
             </ul>
           </div>
         </div>
       </>
-  )
-      ;
+  );
 }
 
 export default Quizzes;
