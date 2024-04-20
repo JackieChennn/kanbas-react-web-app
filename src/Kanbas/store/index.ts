2f3f486888faaac593065d2e8d366abf3c04c2dd
Jackie Chen <cjc337993932@gmail.com>
2024-04-19 23:27:15 -0400

    Added Quiz reducer to store/index.ts

--- src/Kanbas/store/index.ts
+++ src/Kanbas/store/index.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "../Courses/Modules/reducer";
 import assignmentReducer from "../Courses/Assignments/reducer";
+import quizReducer from "../Courses/Quizzes/reducer";
+
 export interface KanbasState {
   modulesReducer: {
     modules: any[];
@@ -11,11 +13,17 @@ export interface KanbasState {
     assignment: any;
     tempAssignment: any;
   };
+  quizReducer: {
+    quizzes: any[];
+    quiz: any;
+    tempQuiz: any;
+  };
 }
 const store = configureStore({
   reducer: {
     modulesReducer,
     assignmentReducer,
+    quizReducer,
   }
 });
  
