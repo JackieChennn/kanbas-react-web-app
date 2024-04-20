df50b15fa4f064f8c81115e1f5eefc350ba9d670
Jackie Chen <cjc337993932@gmail.com>
2024-04-19 23:04:29 -0400

    Implemented Quiz client

---
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = "http://localhost:4000";
+const QUIZZES_API = `${API_BASE}/api/quizzes`;
+export const createQuiz = async (courseId: any, quiz: any) => {
+  const response = await axios.post(
+      QUIZZES_API,
+      quiz
+  );
+  return response.data;
+};
+export const updateQuiz = async (quiz: any) => {
+  const response = await axios
+  .put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  return response.data;
+};
+export const deleteQuiz = async (quizId: any) => {
+  const response = await axios
+  .delete(`${QUIZZES_API}/${quizId}`);
+  return response.data;
+};
+export const findAllQuizzes = async () => {
+  const response = await axios
+  .get(QUIZZES_API);
+  return response.data;
+};
